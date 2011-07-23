271b66c8407d19fdd75f22682ad8bc043a44f036
Philip Cristiano <git@philipcristiano.com>
2011-07-23 13:31:29 -0400

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name = 'jsonrequester',
-    version = '0.1.1',
+    version = '0.1.2',
     description = 'A library for client JSON REST requests',
     packages = find_packages(),
     long_description=read('README.rst'),
