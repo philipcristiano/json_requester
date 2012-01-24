40c132872d8c8a51874a59c745f4373de55ad76c
Patrick Smith <pjs482+github@gmail.com>
2012-01-24 13:51:18 -0500

    tests.unit: Add tests for JsonRequester.request

--- tests/unit/test_json_requestor.py
+++ tests/unit/test_json_requestor.py
@@ -82,3 +82,113 @@ class WhenCreatingJsonRequesterWithTimeout(BaseCreatingJsonRequester):
 
     def should_create_Http_with_timeout(self):
         assert self.module.httplib2.Http.calls('()', timeout=self.timeout)
+
+
+####
+##
+## JsonRequester.request
+##
+####
+
+class BaseRequestTestCase(DingusWhitelistTestCase):
+
+    module = mod
+    module_mocks = ['json', 'httplib2']
+    additional_mocks = [
+        'base_url', 'content', 'kwargs', 'method', 'response', 'url']
+
+    def setup(self):
+        DingusWhitelistTestCase.setup(self)
+        self.old_get_full_url = JsonRequester._get_full_url
+        JsonRequester._get_full_url = Dingus('_get_full_url')
+        self.json_requester = JsonRequester(self.base_url)
+
+    def teardown(self):
+        JsonRequester._get_full_url = self.old_get_full_url
+
+
+class PropertyNoDataProvided(object):
+
+    def should_call_http_request_without_data(self):
+        assert self.module.httplib2.Http().request.calls(
+            '()',
+            JsonRequester._get_full_url(),
+            self.method,
+            body=None,
+            headers={},
+        )
+
+
+class PropertyDataProvided(object):
+
+    def should_call_http_request_with_data(self):
+        assert self.module.httplib2.Http().request.calls(
+            '()',
+            JsonRequester._get_full_url(),
+            self.method,
+            body=self.module.json.dumps(),
+            headers={'Content-Type': 'application/json'},
+        )
+
+
+class WhenRequestReturnsApplicationJsonAndContentIsNotNone(
+        BaseRequestTestCase, PropertyNoDataProvided):
+
+    def setup(self):
+        BaseRequestTestCase.setup(self)
+        self.response['content-type'] = 'application/json'
+        self.module.httplib2.Http().request.return_value = (
+            self.response, self.content)
+
+        self.returned = self.json_requester.request(self.method, self.url)
+
+    def should_return_json(self):
+        assert self.returned == self.module.json.loads()
+
+
+class WhenRequestReturnsApplicationJsonAndContentIsNone(
+        BaseRequestTestCase, PropertyNoDataProvided):
+
+    def setup(self):
+        BaseRequestTestCase.setup(self)
+        self.response['content-type'] = 'application/json'
+        self.module.httplib2.Http().request.return_value = (
+            self.response, None)
+
+        self.returned = self.json_requester.request(self.method, self.url)
+
+    def should_return_None(self):
+        assert self.returned == None
+
+
+class WhenRequestDoesNotReturnApplicationJson(
+        BaseRequestTestCase, PropertyNoDataProvided):
+
+    def setup(self):
+        BaseRequestTestCase.setup(self)
+        self.response['content-type'] = 'not application/json'
+        self.module.httplib2.Http().request.return_value = (
+            self.response, self.content)
+
+        self.returned = self.json_requester.request(self.method, self.url)
+
+    def should_return_None(self):
+        assert self.returned == None
+
+
+class WhenRequestWithDataReturnsApplicationJsonAndContentIsNotNone(
+        BaseRequestTestCase, PropertyDataProvided):
+
+    additional_mocks = ['data']
+
+    def setup(self):
+        BaseRequestTestCase.setup(self)
+        self.response['content-type'] = 'application/json'
+        self.module.httplib2.Http().request.return_value = (
+            self.response, self.content)
+
+        self.returned = self.json_requester.request(
+            self.method, self.url, self.data)
+
+    def should_return_json(self):
+        assert self.returned == self.module.json.loads()
